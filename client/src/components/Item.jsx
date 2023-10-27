303775efa79724b1aba6624d7aa2e8ed6bb9a6bf
psdurbano <114499052+psdurbano@users.noreply.github.com>
2023-10-28 01:08:19 +0200

    transitions & in cart modified

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Box, Typography, useTheme, Button } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 import { shades } from "../theme";
 import { addToCart } from "../state";
 import { useNavigate } from "react-router-dom";
@@ -9,15 +9,10 @@ const Item = ({ item, width }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart.cart);
-
   const [isHovered, setIsHovered] = useState(false);
-  const {
-    palette: { neutral },
-  } = useTheme();
+  const isItemInCart = cart.some((cartItem) => cartItem.id === item.id);
 
-  //Extrae atributos del item
   const { category, price, name, image } = item.attributes;
-
   const {
     data: {
       attributes: {
@@ -28,60 +23,59 @@ const Item = ({ item, width }) => {
     },
   } = image;
 
-  //Verifica si el artículo está en el carrito
-  const isItemInCart = cart.some((cartItem) => cartItem.id === item.id);
+  const handleAddToCart = () => {
+    if (!isItemInCart) {
+      dispatch(addToCart({ item: { ...item, count: 1 } }));
+    }
+  };
 
   return (
-    <Box width={width}>
+    <Box
+      width={width}
+      position="relative"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      style={{
+        transition: "transform 0.3s, box-shadow 0.3s",
+        transform: isHovered ? "scale(1.05)" : "scale(1)",
+        boxShadow: isHovered ? "0 2px 4px rgba(0, 0, 0, 0.1)" : "none",
+      }}
+    >
+      <img
+        alt={item.name}
+        width="380px"
+        height="380px"
+        src={url}
+        onClick={() => navigate(`/item/${item.id}`)}
+        style={{ cursor: "pointer" }}
+      />
       <Box
-        position="relative"
-        onMouseOver={() => setIsHovered(true)}
-        onMouseOut={() => setIsHovered(false)}
+        display={isHovered ? "flex" : "none"}
+        justifyContent="space-between"
+        position="absolute"
+        left="65%"
+        top="-10%"
+        width="100%"
+        padding="0 5%"
+        background={shades.neutral[100]}
+        borderRadius="3px"
+        style={{ position: "relative" }}
       >
-        <img
-          alt={item.name}
-          width="400px"
-          height="400px"
-          src={url}
-          onClick={() => navigate(`/item/${item.id}`)}
-          style={{ cursor: "pointer" }}
-        />
-        <Box
-          display={isHovered ? "block" : "none"}
-          position="absolute"
-          bottom="10%"
-          left="0"
-          width="100%"
-          padding="0 5%"
+        <Button
+          onClick={handleAddToCart}
+          sx={{
+            backgroundColor: isItemInCart ? "#999999" : shades.primary[500],
+            color: "white",
+            cursor: isItemInCart ? "default" : "pointer",
+          }}
+          disabled={isItemInCart}
         >
-          <Box display="flex" justifyContent="space-between">
-            <Box
-              display="flex"
-              alignItems="center"
-              background={shades.neutral[100]}
-              borderRadius="3px"
-            ></Box>
-            <Button
-              onClick={() => {
-                if (!isItemInCart) {
-                  dispatch(addToCart({ item: { ...item, count: 1 } })); //Agrega el artículo al carrito
-                }
-              }}
-              sx={{
-                backgroundColor: isItemInCart ? "#999999" : shades.primary[500],
-                color: "white",
-                cursor: isItemInCart ? "default" : "pointer",
-              }}
-              disabled={isItemInCart} //Deshabilitar el botón si el artículo ya está en el carrito
-            >
-              {isItemInCart ? "In Cart" : "Add to Cart"}
-            </Button>
-          </Box>
-        </Box>
+          {isItemInCart ? "In Cart" : "Add to Cart"}
+        </Button>
       </Box>
 
       <Box mt="3px">
-        <Typography variant="subtitle2" color={neutral.dark}>
+        <Typography variant="subtitle2" color={shades.neutral.dark}>
           {category
             .replace(/([A-Z])/g, " $1")
             .replace(/^./, (str) => str.toUpperCase())}
