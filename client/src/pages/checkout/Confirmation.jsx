1259a4ff7cf515690e88fb490cfb708e06184ac7
psdurbano <114499052+psdurbano@users.noreply.github.com>
2023-10-28 03:52:13 +0200

    confirmation updated

--- client/src/pages/checkout/Confirmation.jsx
+++ client/src/pages/checkout/Confirmation.jsx
@@ -1,8 +1,20 @@
+import React, { useEffect } from "react";
 import { Box } from "@mui/material";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
+import { useNavigate } from "react-router-dom";
 
 const Confirmation = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigate("/");
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [navigate]);
+
   return (
     <Box m="90px auto" width="80%" height="50vh">
       <Alert severity="success">
