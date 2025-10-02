60a7ef0c5cf76d00ce4ca83a73ce2a0fcadf6360
Pablo Durbano <pablosdurbano@gmail.com>
2025-10-02 15:56:57 +0200

    Use connection string to fix IPv6 connectivity issues

--- server/config/database.js
+++ server/config/database.js
@@ -2,13 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      connectionString: env('DATABASE_URL'),
       ssl: env.bool('DATABASE_SSL', true),
-      schema: env('DATABASE_SCHEMA', 'public'),
     },
     debug: false,
   },
