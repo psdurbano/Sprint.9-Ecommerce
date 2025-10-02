d929ccf4353539b4aa1dde3fba0ae963575b1c1f
Pablo Durbano <pablosdurbano@gmail.com>
2025-10-02 16:07:04 +0200

    Fix database connection - support both connection string and individual settings

--- server/config/database.js
+++ server/config/database.js
@@ -1,9 +1,17 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
-    connection: {
+    connection: env('DATABASE_URL') ? {
       connectionString: env('DATABASE_URL'),
+      ssl: { rejectUnauthorized: false },
+    } : {
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', true),
+      schema: env('DATABASE_SCHEMA', 'public'),
     },
     debug: false,
   },
