3a419d1d5f36273aacda579b72ae011e9bcfa310
Pablo Durbano <pablosdurbano@gmail.com>
2025-10-02 16:31:39 +0200

    Fix: update database.js to use SSL configuration with rejectUnauthorized set to false for Supabase

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,22 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
-    connection: env('DATABASE_URL') ? {
-      connectionString: env('DATABASE_URL'),
-      ssl: { rejectUnauthorized: false },
-    } : {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', true),
-      schema: env('DATABASE_SCHEMA', 'public'),
-    },
+    connection: env('DATABASE_URL')
+      ? {
+          connectionString: env('DATABASE_URL'),
+          ssl: { rejectUnauthorized: false },
+        }
+      : {
+          host: env('DATABASE_HOST'),
+          port: env.int('DATABASE_PORT'),
+          database: env('DATABASE_NAME'),
+          user: env('DATABASE_USERNAME'),
+          password: env('DATABASE_PASSWORD'),
+          ssl: env.bool('DATABASE_SSL', true)
+            ? { rejectUnauthorized: false }
+            : false,
+          schema: env('DATABASE_SCHEMA', 'public'),
+        },
     debug: false,
   },
 });
