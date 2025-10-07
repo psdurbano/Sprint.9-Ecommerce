3817dd9df681984b83bbe0bc6ccacff6735fb525
Pablo Durbano <pablosdurbano@gmail.com>
2025-10-07 10:25:45 +0200

    feat: add cloudinary upload provider

--- server/config/plugins.js
+++ server/config/plugins.js
@@ -1,10 +1,16 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'local',
-        providerOptions: {
-          sizeLimit: 1000000,
-        },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-  });
+  },
+});
